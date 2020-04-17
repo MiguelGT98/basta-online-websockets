027713187ce56f0c659202aa4d9735bca60161e0
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-17 12:15:56 -0500

    Funcionalidad básica completa.

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,283 @@
-function makeToast() {
-  $.toast({
-    text: 'This is called a toast!',
-    position: 'top-right'
-  });
-}
-
-function makeToastMessage(message) {
-  $.toast({
-    text: message,
-    position: 'top-right'
-  });
-}
-
 window.socket = null;
+
 function connectToSocketIo() {
   let server = window.location.protocol + "//" + window.location.host;
   window.socket = io.connect(server);
-  // Recibe un mensaje de tipo toast
-  window.socket.on('toast', function (data) {
-    // Muestra el mensaje
-    makeToastMessage(data.message);
+
+  hideStopButton();
+  hideConnectButton();
+  hideReconnectButton();
+
+  window.socket.on("basta", function (player) {
+    startCounter(player);
+  });
+
+  window.socket.on("starting", function () {
+    startCountdownToGameStart();
+  });
+
+  window.socket.on("players", function (players) {
+    showLobby(players);
+  });
+
+  window.socket.on("queue", function (players) {
+    showQueueLobby(players);
+  });
+
+  window.socket.on("started", function (gameData) {
+    setupGame(gameData);
+  });
+
+  window.socket.on("greetings", function (player) {
+    window.player = player;
+    const greetingsHeading = document.getElementById("greetings");
+    greetingsHeading.innerHTML = `Welcome ${player.name}`;
+  });
+
+  window.socket.on("answersReady", function (players) {
+    window.players = players;
+    showAllAnswers();
   });
 }
 
-function emitEventToSocketIo() {
-  let text = $('#messageToServer').val();
-  // Envía un mensaje
-  window.socket.emit('messageToServer', { text: text });
+function emitBasta() {
+  window.socket.emit("basta", { player: window.player });
 }
 
-$(function () {
-  connectToSocketIo();
-});
+function showLobby(players) {
+  hideStopButton();
+  hideConnectButton();
+  hideReconnectButton();
+
+  const playersContainer = document.getElementById("players");
+  playersContainer.innerHTML = "";
+
+  for (key in players) {
+    const newNode = document.createElement("div");
+    newNode.innerHTML = players[key].name;
+    playersContainer.appendChild(newNode);
+  }
+}
+
+function showQueueLobby(players) {
+  hideStopButton();
+  hideConnectButton();
+  hideReconnectButton();
+
+  const playersContainer = document.getElementById("players");
+  playersContainer.innerHTML = "";
+
+  const newNode = document.createElement("div");
+  newNode.innerHTML = "Esperando a que termine el juego";
+  playersContainer.appendChild(newNode);
+
+  for (key in players) {
+    const newNode = document.createElement("div");
+    newNode.innerHTML = players[key].name;
+    playersContainer.appendChild(newNode);
+  }
+}
+
+function setupGame({ players, letter, questions }) {
+  window.players = players;
+  window.letter = letter;
+  window.questions = questions;
+
+  const greetingsHeading = document.getElementById("greetings");
+  greetingsHeading.innerHTML = `La letra es ${letter}, ${player.name}`;
+
+  const playersContainer = document.getElementById("players");
+  playersContainer.innerHTML = "";
+
+  const headerNode = document.createElement("div");
+  headerNode.classList.toggle("input-group");
+  playersContainer.appendChild(headerNode);
+
+  const name = document.createElement("div");
+  name.classList.toggle("input-group-prepend");
+  name.innerHTML = `<span class="input-group-text">Nombre del jugador</span>`;
+  headerNode.appendChild(name);
+
+  questions.forEach((question, index) => {
+    const inputNode = document.createElement("input");
+    inputNode.type = "text";
+    inputNode.classList.toggle("form-control");
+    inputNode.disabled = true;
+    inputNode.value = question;
+    headerNode.appendChild(inputNode);
+  });
+
+  for (key in players) {
+    const newNode = document.createElement("div");
+    newNode.classList.toggle("input-group");
+    playersContainer.appendChild(newNode);
+
+    const titleNode = document.createElement("div");
+    titleNode.classList.toggle("input-group-prepend");
+    titleNode.innerHTML = `<span class="input-group-text">${players[key].name}</span>`;
+    newNode.appendChild(titleNode);
+
+    questions.forEach((question) => {
+      const inputNode = document.createElement("input");
+      inputNode.type = "text";
+      inputNode.name = question;
+      inputNode.placeholder = question;
+      inputNode.classList.toggle("form-control");
+
+      if (window.player.id == key) {
+        inputNode.id = `q-${question}`;
+      }
+
+      if (window.player.id != key) {
+        inputNode.disabled = true;
+      }
+
+      newNode.appendChild(inputNode);
+    });
+  }
+
+  showStopButton();
+  hideConnectButton();
+  hideReconnectButton();
+}
+
+function showAllAnswers() {
+  const players = window.players;
+  const questions = window.questions;
+
+  const playersContainer = document.getElementById("players");
+  playersContainer.innerHTML = "";
+
+  const headerNode = document.createElement("div");
+  headerNode.classList.toggle("input-group");
+  playersContainer.appendChild(headerNode);
+
+  const name = document.createElement("div");
+  name.classList.toggle("input-group-prepend");
+  name.innerHTML = `<span class="input-group-text">Nombre del jugador</span>`;
+  headerNode.appendChild(name);
+
+  questions.forEach((question, index) => {
+    const inputNode = document.createElement("input");
+    inputNode.type = "text";
+    inputNode.classList.toggle("form-control");
+    inputNode.disabled = true;
+    inputNode.value = question;
+    headerNode.appendChild(inputNode);
+  });
+
+  for (key in players) {
+    const newNode = document.createElement("div");
+    newNode.classList.toggle("input-group");
+    playersContainer.appendChild(newNode);
+
+    const titleNode = document.createElement("div");
+    titleNode.classList.toggle("input-group-prepend");
+    titleNode.innerHTML = `<span class="input-group-text">${players[key].name}</span>`;
+    newNode.appendChild(titleNode);
+
+    questions.forEach((question, index) => {
+      const inputNode = document.createElement("input");
+      inputNode.type = "text";
+      inputNode.name = question;
+      inputNode.placeholder = question;
+      inputNode.classList.toggle("form-control");
+      inputNode.disabled = true;
+      inputNode.value = `${players[key].answers[index].answer} (${players[key].answers[index].points})`;
+
+      newNode.appendChild(inputNode);
+    });
+  }
+
+  hideStopButton();
+  hideConnectButton();
+  showReconnectButton();
+}
+
+async function startCounter(player) {
+  showAlertCounter();
+  const counterDiv = document.getElementById("counter");
+  const stopButton = document.getElementById("stop");
+  stopButton.classList.toggle("hidden");
+
+  for (let i = 1; i <= 10; i++) {
+    counterDiv.innerHTML = `Basta ${i} de ${player.name}`;
+    await sleep(1000);
+  }
+
+  counterDiv.innerHTML = "";
+  hideAlertCounter();
+
+  window.socket.emit("submission", {
+    player: window.player,
+    answers: window.questions.map((question) => {
+      const questionInput = document.getElementById(`q-${question}`);
+      return questionInput.value;
+    }),
+  });
+}
+
+async function startCountdownToGameStart() {
+  showAlertCounter();
+  const counterDiv = document.getElementById("counter");
+
+  for (let i = 5; i >= 1; i--) {
+    counterDiv.innerHTML = `El juego comienza en ${i} segundos`;
+    await sleep(1000);
+  }
+
+  hideAlertCounter();
+  counterDiv.innerHTML = "";
+}
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const closeResults = () => {
+  // Reset the game
+  const greetingsHeading = document.getElementById("greetings");
+  greetingsHeading.innerHTML = `Ingresa al juego`;
+  const stopButton = document.getElementById("stop");
+  stopButton.classList.toggle("hidden");
+};
+
+const hideStopButton = () => {
+  const stopButton = document.getElementById("stop");
+  stopButton.classList.add("hidden");
+};
+
+const showStopButton = () => {
+  const stopButton = document.getElementById("stop");
+  stopButton.classList.remove("hidden");
+};
+
+const hideReconnectButton = () => {
+  const stopButton = document.getElementById("reconnect");
+  stopButton.classList.add("hidden");
+};
+
+const showReconnectButton = () => {
+  const stopButton = document.getElementById("reconnect");
+  stopButton.classList.remove("hidden");
+};
+
+const hideConnectButton = () => {
+  const stopButton = document.getElementById("connect");
+  stopButton.classList.add("hidden");
+};
+
+const showConnectButton = () => {
+  const stopButton = document.getElementById("connect");
+  stopButton.classList.remove("hidden");
+};
+
+const hideAlertCounter = () => {
+  const stopButton = document.getElementById("counter");
+  stopButton.classList.add("hidden");
+};
+
+const showAlertCounter = () => {
+  const stopButton = document.getElementById("counter");
+  stopButton.classList.remove("hidden");
+};
--- server.js
+++ server.js
@@ -1,53 +1,248 @@
+const util = require("util");
+
 // Imports
-const express = require('express');
-const webRoutes = require('./routes/web');
+const express = require("express");
+const webRoutes = require("./routes/web");
 
 // Express app creation
 const app = express();
 
 // Connect socket io
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 // Configurations
-const appConfig = require('./configs/app');
+const appConfig = require("./configs/app");
 
 // View engine configs
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbshelpers = require("handlebars-helpers");
 const multihelpers = hbshelpers();
-const extNameHbs = 'hbs';
+const extNameHbs = "hbs";
 const hbs = exphbs.create({
   extname: extNameHbs,
-  helpers: multihelpers
+  helpers: multihelpers,
 });
 app.engine(extNameHbs, hbs.engine);
-app.set('view engine', extNameHbs);
+app.set("view engine", extNameHbs);
 
 // Receive parameters from the Form requests
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
 // Routes
-app.use('/', webRoutes);
+app.use("/", webRoutes);
 
-io.on('connection', (socket) => {
-  // Recibe la conexión del cliente
-  console.log('Client connected...');
-  let i = 0;
-  // Emite un mensaje
-  setInterval(() => {
-    socket.emit('toast', { message: `Message: ${i}`});
-    i++;
-  }, 3000);
-  // Recibe un mensaje
-  socket.on('messageToServer', (data) => {
-    console.log('messageReceivedFromClient: ', data.text);
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+const randomNames = [
+  "Iola Cinnamon",
+  "Janelle Opie",
+  "Shawana Blattner",
+  "Kenneth Wille",
+  "Les Daniele",
+  "Al Alger",
+  "Magdalene Loyola",
+  "Karole Keane",
+  "Scot Stoller",
+  "Elizabet Corbeil",
+  "Jeffrey Armitage",
+  "Bianca Standish",
+];
+
+let players = {};
+let playersQueue = {};
+let connections = [];
+let connectionsInQueue = [];
+
+const questions = ["Nombre", "Ciudad", "Animal", "Fruto"];
+const letters = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+let letter;
+
+let gameInProgress = false;
+let notificationSent = false;
+
+io.on("connection", (socket) => {
+  if (gameInProgress) {
+    connectionsInQueue.push(socket);
+    const name = randomNames.pop();
+    playersQueue[socket.id] = { name, id: socket.id };
+    socket.emit("greetings", playersQueue[socket.id]);
+    connectionsInQueue.forEach((socket) => {
+      socket.emit("queue", playersQueue);
+    });
+  } else {
+    connections.push(socket);
+    shuffle(randomNames);
+    // Recibe la conexión del cliente
+    const name = randomNames.pop();
+    players[socket.id] = { name, id: socket.id };
+    socket.emit("greetings", players[socket.id]);
+
+    connections.forEach((socket) => {
+      socket.emit("players", players);
+    });
+  }
+
+  if (Object.keys(players).length >= 2 && !notificationSent) {
+    startGame();
+  }
+
+  socket.on("disconnect", function () {
+    var i = connections.indexOf(socket);
+
+    if (i !== -1) {
+      connections.splice(i, 1);
+      randomNames.push(players[socket.id].name);
+      delete players[socket.id];
+    }
+
+    i = connectionsInQueue.indexOf(socket);
+    if (i !== -1) {
+      connectionsInQueue.splice(i, 1);
+      randomNames.push(players[socket.id].name);
+      delete playersQueue[socket.id];
+    }
+
+    if (connections.length == 0) {
+      restartGame();
+    }
+  });
+
+  socket.on("basta", ({ player }) => {
+    connections.forEach((socket) => {
+      socket.emit("basta", player);
+    });
+  });
+
+  socket.on("submission", function ({ player, answers }) {
+    players[player.id].answers = answers;
+
+    let wait = false;
+    for (key in players) {
+      if (players[key].answers == null) {
+        wait = true;
+      }
+    }
+
+    if (!wait) {
+      calculateResults();
+
+      connections.forEach((socket) => {
+        socket.emit("answersReady", players);
+      });
+
+      restartGame();
+    }
   });
 });
 
+const restartGame = () => {
+  gameInProgress = false;
+  notificationSent = false;
+
+  for (let key in players) {
+    randomNames.push(players[key].name);
+  }
+
+  players = playersQueue;
+  playersQueue = {};
+  connections = connectionsInQueue;
+  connectionsInQueue = [];
+
+  connections.forEach((socket) => {
+    socket.emit("players", players);
+  });
+};
+
+const calculateResults = () => {
+  let questionsWithAnswer = [];
+  questions.forEach((question, index) => {
+    let answersForThisQuestion = [];
+
+    for (key in players) {
+      answersForThisQuestion.push({
+        player: { ...players[key], answers: null },
+        answer: players[key].answers[index],
+      });
+    }
+
+    questionsWithAnswer.push({
+      question: question,
+      answers: answersForThisQuestion,
+    });
+  });
+
+  questionsWithAnswer.forEach((question) => {
+    question.answers.forEach((answer) => {
+      if (answer.answer === "" || answer.answer[0].toUpperCase() !== letter) {
+        answer.points = 0;
+      } else {
+        let repeated = 1;
+
+        question.answers.forEach((otherAnswer, index) => {
+          if (answer.player.id === otherAnswer.player.id) {
+          } else if (answer.answer === otherAnswer.answer) repeated++;
+        });
+
+        if (repeated > 1) {
+          answer.points = Math.floor(100 / repeated);
+        } else {
+          answer.points = 100;
+        }
+      }
+    });
+  });
+
+  questionsWithAnswer.forEach((question, index) => {
+    question.answers.forEach((answer) => {
+      players[answer.player.id].answers[index] = {
+        answer: answer.answer,
+        points: answer.points,
+      };
+    });
+  });
+};
+
+const startGame = () => {
+  connections.forEach((socket) => {
+    socket.emit("starting");
+  });
+
+  notificationSent = true;
+
+  setTimeout(() => {
+    shuffle(letters);
+    letter = letters.pop();
+    connections.forEach((socket) => {
+      socket.emit("started", { players, letter, questions });
+    });
+    gameInProgress = true;
+  }, 5000);
+};
+
 // App init
 server.listen(appConfig.expressPort, () => {
-  console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+  console.log(
+    `Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`
+  );
 });
